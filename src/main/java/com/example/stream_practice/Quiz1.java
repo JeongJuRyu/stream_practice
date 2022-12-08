ef42c36adcf4de91832a41f494c62bd15114b331
ryujeongju <jeongju_ryu@tmax.co.kr>
2022-12-08 09:43:29 +0900

    Solve Quiz2

--- src/main/java/com/example/stream_practice/Quiz1.java
+++ src/main/java/com/example/stream_practice/Quiz1.java
@@ -15,11 +15,6 @@ public class Quiz1 {
     // 1.1 각 취미를 선호하는 인원이 몇 명인지 계산하여라.
     public Map<String, Integer> quiz1() throws IOException {
         List<String[]> csvLines = readCsvLines();
-        for(String[] lines : csvLines){
-            for(String line : lines){
-                System.out.println(line);
-            }
-        }
         return csvLines.stream()
                 .map(line -> line[1].replaceAll("\\s",""))
                 .flatMap(hobbies -> Arrays.stream(hobbies.split(":")))
@@ -29,7 +24,11 @@ public class Quiz1 {
     // 1.2 각 취미를 선호하는 정씨 성을 갖는 인원이 몇 명인지 계산하여라.
     public Map<String, Integer> quiz2() throws IOException {
         List<String[]> csvLines = readCsvLines();
-        return new HashMap<>();
+        return csvLines.stream()
+                .filter(line -> line[0].startsWith("정"))
+                .map(line -> line[1].replaceAll("\\s", ""))
+                .flatMap(hobbies -> Arrays.stream(hobbies.split(":")))
+                .collect(Collectors.toMap(hobby -> hobby, hobby -> 1, (oldValue, newValue) -> ++newValue));
     }
 
     // 1.3 소개 내용에 '좋아'가 몇번 등장하는지 계산하여라.
--- src/main/java/com/example/stream_practice/StreamPracticeApplication.java
+++ src/main/java/com/example/stream_practice/StreamPracticeApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import java.io.IOException;
+import java.util.Map;
 
 @SpringBootApplication
 public class StreamPracticeApplication {
@@ -11,7 +12,15 @@ public class StreamPracticeApplication {
     public static void main(String[] args) throws IOException {
         SpringApplication.run(StreamPracticeApplication.class, args);
         Quiz1 quiz1 = new Quiz1();
-        quiz1.quiz1();
+        Map<String, Integer> answer = quiz1.quiz1();
+
+        for(String key : answer.keySet()){
+            System.out.println(key + " : " + answer.get(key));
+        }
+        answer = quiz1.quiz2();
+        for(String key : answer.keySet()){
+            System.out.println(key + " : " + answer.get(key));
+        }
     }
 
 }
