10b4570923d5a04350de521fe491c82b12274f41
ryujeongju <jeongju_ryu@tmax.co.kr>
2022-12-10 14:49:30 +0900

    Add Quiz2-1

---
+++ src/main/java/com/example/stream_practice/Quiz2.java
@@ -0,0 +1,16 @@
+package com.example.stream_practice;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class Quiz2 {
+    private static List<String> WORDS = Arrays.asList("TONY", "a", "hULK", "B", "america", "X", "nebula", "Korea");
+
+    // 2.1 List에 저장된 단어들의 접두사가 각각 몇개씩 있는지 Map<String, Integer>으로 변환하여라.
+    // ex) ("T", 1), ("a", 2) ...
+    public Map<String, Integer> quiz1(){
+        return new HashMap<>();
+    }
+}
