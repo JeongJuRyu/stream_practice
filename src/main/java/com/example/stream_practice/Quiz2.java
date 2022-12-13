fe5a9582271a164497cddb745d647d6f65d89ba9
jeongjuryu <totw2018@naver.com>
2022-12-13 21:17:39 +0900

    solve Quiz2-1

--- src/main/java/com/example/stream_practice/Quiz2.java
+++ src/main/java/com/example/stream_practice/Quiz2.java
@@ -4,14 +4,16 @@ import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 public class Quiz2 {
-    private static List<String> WORDS = Arrays.asList("TONY", "a", "hULK", "B", "america", "X", "nebula", "Korea");
+    private static List<String> words = Arrays.asList("TONY", "a", "hULK", "B", "america", "X", "nebula", "Korea");
 
     // 2.1 List에 저장된 단어들의 접두사가 각각 몇개씩 있는지 Map<String, Integer>으로 변환하여라.
     // ex) ("T", 1), ("a", 2) ...
 
     public Map<String, Integer> quiz1(){
-        return new HashMap<>();
+        return words.stream().map(s -> s.substring(0,1))
+                .collect(Collectors.toMap(s -> s, s -> 1, (oldValue, newValue) -> newValue += oldValue));
     }
 }
--- src/main/java/com/example/stream_practice/StreamPracticeApplication.java
+++ src/main/java/com/example/stream_practice/StreamPracticeApplication.java
@@ -11,19 +11,21 @@ public class StreamPracticeApplication {
 
     public static void main(String[] args) throws IOException {
         SpringApplication.run(StreamPracticeApplication.class, args);
-        Quiz1 quiz1 = new Quiz1();
-        Map<String, Integer> answer = quiz1.quiz1();
+//        Quiz1 quiz1 = new Quiz1();
+//        Map<String, Integer> answer = quiz1.quiz1();
+//
+//        for(String key : answer.keySet()){
+//            System.out.println(key + " : " + answer.get(key));
+//        }
+//        answer = quiz1.quiz2();
+//        for(String key : answer.keySet()){
+//            System.out.println(key + " : " + answer.get(key));
+//        }
+//
+//        int quiz3Answer = quiz1.quiz3();
+//        System.out.println(quiz3Answer);
 
-        for(String key : answer.keySet()){
-            System.out.println(key + " : " + answer.get(key));
-        }
-        answer = quiz1.quiz2();
-        for(String key : answer.keySet()){
-            System.out.println(key + " : " + answer.get(key));
-        }
-
-        int quiz3Answer = quiz1.quiz3();
-        System.out.println(quiz3Answer);
+        Quiz2 quiz2 = new Quiz2();
+        System.out.println(quiz2.quiz1());
     }
-
 }
